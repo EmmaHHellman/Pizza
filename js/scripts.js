b565951b5910c29665f3071dd8a27f0ba9d606aa
Emma Hellman <ehh801@icloud.com>
2020-06-19 09:31:33 -0700

    add changes to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,34 @@
+function Pizza(toppings, size) {
+  this.toppings = toppings,
+  this.size = size
+}
+
 Pizza.prototype.priceAdj = function() {
   let pizzaPrice;
   if (pizzaPrice = 10) {
     if (this.size === "Small") {
       pizzaPrice -= 1;
-    } else if (this.size === "Large"){
+    } else if (this.size === "Large") {
       pizzaPrice += 1;
-    } else if (this.size === "medium" ){
+    } else if (this.size === "medium" ) {
       pizzaPrice -= 0;
     }
+  }
 
+  var selectedToppings = $('#toppings').val();
 
-function Pizza(toppings, size) {
-  this.toppings = toppings,
-  this.size = size
+  for (let i = 0; i < selectedToppings; i++) {
+    if(selectedToppings[i].length > 2) {
+      if (selectedToppings[i] === true) {
+        pizzaPrice -= .5;
+      }
+    } else {
+      pizzaPrice -= 0;
+    }
+  }
+
+  return pizzaPrice;
+  
 }
 
 $(document).ready(function() {
